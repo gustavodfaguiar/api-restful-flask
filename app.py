c18ac8b5dae7ec7442ff9e0c75ca4e9bd1785a48
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-08-17 15:42:46 -0300

    Add class ItemList

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 
@@ -16,12 +16,19 @@ class Item(Resource):
         return {'item': None}, 404
 
     def post(self, name):
-        item = {'name': name, 'price': 12.00}
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
 
 
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+
 api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
 
 
 if __name__ == '__main__':
